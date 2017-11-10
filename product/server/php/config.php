b2eae71c6b998d427c6c33d41788d081bf05697e
diana4545 <diana4545@outlook.com>
2017-11-10 07:28:31 +0100

    create project

---
+++ product/server/php/config.php
@@ -0,0 +1,12 @@
+<?php
+//turn on php error reporting
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
+
+define( 'ORDER_FOLDER', 'orders' );
+define( 'IMAGE_FOLDER', 'uploaded_images' );
+
+header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Methods: POST");
+
+?>
