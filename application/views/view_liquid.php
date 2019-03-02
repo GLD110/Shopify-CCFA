9d7ac1353aa6d832671e85dfee68957e69c40d60
diana4545 <diana4545@outlook.com>
2019-03-02 06:35:35 +0000

    update

--- application/views/view_liquid.php
+++ application/views/view_liquid.php
@@ -91,72 +91,72 @@ foreach( array_keys( $arrContent ) as $key ) {
 
     <script>
 
-//       var Base64={_keyStr:"ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/=",encode:function(e){var t="";var n,r,i,s,o,u,a;var f=0;e=Base64._utf8_encode(e);while(f<e.length){n=e.charCodeAt(f++);r=e.charCodeAt(f++);i=e.charCodeAt(f++);s=n>>2;o=(n&3)<<4|r>>4;u=(r&15)<<2|i>>6;a=i&63;if(isNaN(r)){u=a=64}else if(isNaN(i)){a=64}t=t+this._keyStr.charAt(s)+this._keyStr.charAt(o)+this._keyStr.charAt(u)+this._keyStr.charAt(a)}return t},decode:function(e){var t="";var n,r,i;var s,o,u,a;var f=0;e=e.replace(/[^A-Za-z0-9+/=]/g,"");while(f<e.length){s=this._keyStr.indexOf(e.charAt(f++));o=this._keyStr.indexOf(e.charAt(f++));u=this._keyStr.indexOf(e.charAt(f++));a=this._keyStr.indexOf(e.charAt(f++));n=s<<2|o>>4;r=(o&15)<<4|u>>2;i=(u&3)<<6|a;t=t+String.fromCharCode(n);if(u!=64){t=t+String.fromCharCode(r)}if(a!=64){t=t+String.fromCharCode(i)}}t=Base64._utf8_decode(t);return t},_utf8_encode:function(e){e=e.replace(/rn/g,"n");var t="";for(var n=0;n<e.length;n++){var r=e.charCodeAt(n);if(r<128){t+=String.fromCharCode(r)}else if(r>127&&r<2048){t+=String.fromCharCode(r>>6|192);t+=String.fromCharCode(r&63|128)}else{t+=String.fromCharCode(r>>12|224);t+=String.fromCharCode(r>>6&63|128);t+=String.fromCharCode(r&63|128)}}return t},_utf8_decode:function(e){var t="";var n=0;var r=c1=c2=0;while(n<e.length){r=e.charCodeAt(n);if(r<128){t+=String.fromCharCode(r);n++}else if(r>191&&r<224){c2=e.charCodeAt(n+1);t+=String.fromCharCode((r&31)<<6|c2&63);n+=2}else{c2=e.charCodeAt(n+1);c3=e.charCodeAt(n+2);t+=String.fromCharCode((r&15)<<12|(c2&63)<<6|c3&63);n+=3}}return t}}
-//
-//       $(document).ready(function () {
-//
-//         // Show the first version
-//         if( $('.content_item').length > 0 ){
-//           $('#txtContent').val( Base64.decode($('.content_item').first().html()));
-//         }
-//
-//         // Version selection
-//         $('#sel_version').change( function(){
-//           $('#txtContent').val( Base64.decode($('.content_item[version="' + $(this).val() + '"]').html() ));
-//           $('#sel_versoin_display').html($(this).val());
-//         });
-//
-//         // type click
-//         $('.btn_schema_type').click( function(){
-//           $('#sel_type').val( $(this).attr('schema_type'));
-//           $('#frmSearch').submit();
-//         });
-//
-//         // Submit
-//         $('.btn_update').click( function(){
-//           if( confirm( 'Would you like to save this update to the new version ?') )
-//           {
-// //            $('#submit_content').val( Base64.encode($('#txtContent').val()));
-//             $('#submit_content').val( $('#txtContent').val());
-//             $('#frmSubmit').submit();
-//           }
-//         });
-//
-//         // Publish
-//         // Sync Button Config
-//         $('.btn_publish').btn_init(
-//           'publish',
-//           {class: 'btn-warning', caption: 'Sync'},
-//           {class: 'btn-default fa fa-spinner', caption: ''},
-//           {class: 'btn-success', caption: 'Done'},
-//           {class: 'btn-danger', caption: 'Error'}
-//         );
-//
-//         $('.btn_publish').click( function(){
-//
-//           $(this).btn_action('publish', 'pending');
-//
-//           $.ajax({
-//             url: '<?php echo base_url($this->config->item('index_page') . '/liquid/publish/' . $sel_type); ?>/' + $('#sel_version').val(),
-//             type: 'GET'
-//           }).done(function (data) {
-//             $('.btn_publish').btn_action('publish', 'success');
-//           }).error( function( data ){
-//             $('.btn_publish').btn_action('publish', 'error');
-//           })
-//         });
-//         $('.btn_publish_all').click( function(){
-//
-//           $(this).btn_action('publish', 'pending');
-//
-//           $.ajax({
-//             url: '<?php echo base_url($this->config->item('index_page') . '/liquid/publish'); ?>',
-//             type: 'GET'
-//           }).done(function (data) {
-//             $('.btn_publish_all').btn_action('publish', 'success');
-//           }).error( function( data ){
-//             $('.btn_publish_all').btn_action('publish', 'error');
-//           })
-//         });
-//       });
+      var Base64={_keyStr:"ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/=",encode:function(e){var t="";var n,r,i,s,o,u,a;var f=0;e=Base64._utf8_encode(e);while(f<e.length){n=e.charCodeAt(f++);r=e.charCodeAt(f++);i=e.charCodeAt(f++);s=n>>2;o=(n&3)<<4|r>>4;u=(r&15)<<2|i>>6;a=i&63;if(isNaN(r)){u=a=64}else if(isNaN(i)){a=64}t=t+this._keyStr.charAt(s)+this._keyStr.charAt(o)+this._keyStr.charAt(u)+this._keyStr.charAt(a)}return t},decode:function(e){var t="";var n,r,i;var s,o,u,a;var f=0;e=e.replace(/[^A-Za-z0-9+/=]/g,"");while(f<e.length){s=this._keyStr.indexOf(e.charAt(f++));o=this._keyStr.indexOf(e.charAt(f++));u=this._keyStr.indexOf(e.charAt(f++));a=this._keyStr.indexOf(e.charAt(f++));n=s<<2|o>>4;r=(o&15)<<4|u>>2;i=(u&3)<<6|a;t=t+String.fromCharCode(n);if(u!=64){t=t+String.fromCharCode(r)}if(a!=64){t=t+String.fromCharCode(i)}}t=Base64._utf8_decode(t);return t},_utf8_encode:function(e){e=e.replace(/rn/g,"n");var t="";for(var n=0;n<e.length;n++){var r=e.charCodeAt(n);if(r<128){t+=String.fromCharCode(r)}else if(r>127&&r<2048){t+=String.fromCharCode(r>>6|192);t+=String.fromCharCode(r&63|128)}else{t+=String.fromCharCode(r>>12|224);t+=String.fromCharCode(r>>6&63|128);t+=String.fromCharCode(r&63|128)}}return t},_utf8_decode:function(e){var t="";var n=0;var r=c1=c2=0;while(n<e.length){r=e.charCodeAt(n);if(r<128){t+=String.fromCharCode(r);n++}else if(r>191&&r<224){c2=e.charCodeAt(n+1);t+=String.fromCharCode((r&31)<<6|c2&63);n+=2}else{c2=e.charCodeAt(n+1);c3=e.charCodeAt(n+2);t+=String.fromCharCode((r&15)<<12|(c2&63)<<6|c3&63);n+=3}}return t}}
+
+      $(document).ready(function () {
+
+        // Show the first version
+        if( $('.content_item').length > 0 ){
+          $('#txtContent').val( Base64.decode($('.content_item').first().html()));
+        }
+
+        // Version selection
+        $('#sel_version').change( function(){
+          $('#txtContent').val( Base64.decode($('.content_item[version="' + $(this).val() + '"]').html() ));
+          $('#sel_versoin_display').html($(this).val());
+        });
+
+        // type click
+        $('.btn_schema_type').click( function(){
+          $('#sel_type').val( $(this).attr('schema_type'));
+          $('#frmSearch').submit();
+        });
+
+        // Submit
+        $('.btn_update').click( function(){
+          if( confirm( 'Would you like to save this update to the new version ?') )
+          {
+//            $('#submit_content').val( Base64.encode($('#txtContent').val()));
+            $('#submit_content').val( $('#txtContent').val());
+            $('#frmSubmit').submit();
+          }
+        });
+
+        // Publish
+        // Sync Button Config
+        $('.btn_publish').btn_init(
+          'publish',
+          {class: 'btn-warning', caption: 'Sync'},
+          {class: 'btn-default fa fa-spinner', caption: ''},
+          {class: 'btn-success', caption: 'Done'},
+          {class: 'btn-danger', caption: 'Error'}
+        );
+
+        $('.btn_publish').click( function(){
+
+          $(this).btn_action('publish', 'pending');
+
+          $.ajax({
+            url: '<?php echo base_url($this->config->item('index_page') . '/liquid/publish/' . $sel_type); ?>/' + $('#sel_version').val(),
+            type: 'GET'
+          }).done(function (data) {
+            $('.btn_publish').btn_action('publish', 'success');
+          }).error( function( data ){
+            $('.btn_publish').btn_action('publish', 'error');
+          })
+        });
+        $('.btn_publish_all').click( function(){
+
+          $(this).btn_action('publish', 'pending');
+
+          $.ajax({
+            url: '<?php echo base_url($this->config->item('index_page') . '/liquid/publish'); ?>',
+            type: 'GET'
+          }).done(function (data) {
+            $('.btn_publish_all').btn_action('publish', 'success');
+          }).error( function( data ){
+            $('.btn_publish_all').btn_action('publish', 'error');
+          })
+        });
+      });
     </script>
